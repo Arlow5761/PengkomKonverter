73d73559e86272538a010ba45111f47233ba4ae8
liatryingtocode <you@example.com>
2023-10-24 13:01:00 +0700

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import Convert
-def Convert():
+import transcribe
+import Downloader
+def Converter():
+    print("Option 1: Convert selected")
     file_name = input("Enter the file name: ")
     output_format = input("Enter the desired format (e.g., jpg, pdf, etc.): ")
-    print("Option 1: Convert selected")
+    output_file = file_name.split(".") [0] + output_format
+    file_group = Convert. GetFileGroup (file_name)
+    if file_group == Convert. GetFileGroup (output_file):
+        if file_group == "Image":
+            Convert. ConvertImage (file_name, output_file)
+        elif file_group  == "Audio":
+            Convert. ConvertAudio (file_name, output_file)
+        elif file_group  == "Video":
+            Convert. ConvertVideo (file_name, output_file)
+    else:
+        if file_group == "Video" and Convert. GetFileGroup (output_file) == "Audio":
+            Convert. ConvertVideoToAudio (file_name, output_file)
+        elif file_group == "Image" and Convert. GetFileGroup (output_file) == "PDF":
+            Convert. ConvertImageToPdf (file_name, output_file)
+        elif file_group == "Docx" and Convert. GetFileGroup (output_file) == "PDF":
+            if Convert. ConvertDocxtoPdf (file_name, output_file) == False:
+                print("error")
+    print("Convert finished")
+
 def Transcribe():
     print("Option 2: Transcribe selected")
+    file_name = input("Enter the file name: ")
+    output_format = input("Enter the desired output file name: ")
+    transcribe. transcribe (file_name, output_format)
+    print("Transcribe finished")
 
 def Download():
     print("Option 3: Download selected")
+    Downloader.Download ()
+    print("Download finished")
 
 selection = True
 while selection:
@@ -19,7 +46,7 @@ while selection:
     
     choice = input("Enter your choice (1/2/3/4): ")
     if choice == '1':
-        (Convert())
+        (Converter())
     elif choice == '2':
         (Transcribe())
     elif choice == '3':
